e169a1572edbb9186b3df4e744b0c4ccdc92537b
Yacou Keita <yacoukeita0212@gmail.com>
2024-10-09 10:15:55 +0000

    this commit redesign filter option

--- src/app/modules/task/presentation/pages/components/task-header/task-header.component.ts
+++ src/app/modules/task/presentation/pages/components/task-header/task-header.component.ts
@@ -4,13 +4,17 @@ import { AsyncPipe, NgClass } from '@angular/common';
 import { Observable } from 'rxjs';
 import { AppState, selectTaskListLength } from '../../../states-management/task.selector';
 import { getTaskList } from '../../../states-management/task.action';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-header',
   standalone: true,
   imports: [
+    DropdownModule,
     AsyncPipe,
     NgClass,
+    ReactiveFormsModule
   ],
   templateUrl: './task-header.component.html',
   styleUrl: './task-header.component.css'
@@ -19,17 +23,22 @@ export class TaskHeaderComponent {
 
   constructor(private readonly store: Store<AppState>) { }
 
-  filterOption?: boolean
+  filterOption = new FormControl<boolean | undefined>(undefined, { nonNullable: true })
+  filterOptionList = [
+    { value: undefined, label: "Toutes les tâches" },
+    { value: true, label: "Tâches terminées" },
+    { value: false, label: "Tâches Incomplètes" },
+  ]
   taskListLength$: Observable<number> = this.store.select(selectTaskListLength)
-  
 
-  runGetTaskList(isFinish?: boolean) {
-    this.filterOption = isFinish
+
+  runGetTaskList() {
+    const isFinish = this.filterOption.value
     this.store.dispatch(getTaskList({ isFinish }))
   }
 
-  isActiveItem(filterOption?:boolean) {
-    return this.filterOption === filterOption
+  isActiveItem(filterOption?: boolean) {
+    return this.filterOption.value === filterOption
   }
 
 }
